b5bf78bd5ab734d999cfa062442ca55aea093ec6
Jack <jdcorley@gmail.com>
2018-12-26 21:08:57 -0500

    nested a new dashboard route to be inside of admin route and manually added posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end 
+
   get 'about', to: 'pages#about'
 
   get 'leadgen/advertising/something_random/randomness', to: 'pages#contact', as: 'lead'
 
   resources :blogs
+  resources :posts
+
+  root to: 'pages#home'
 end
